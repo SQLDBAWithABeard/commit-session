7d0210281f501db666de2239346ed3219bbb0be2
Rob Sewell <mrrobsewell@outlook.com>
2025-04-16 16:50:20 +0100

    Add script to generate random commit messages
    This commit introduces a PowerShell script that creates files with random commit messages from a predefined list. This will help in testing and simulating various commit scenarios. Great job on enhancing the repository's functionality, and David Letterman is proud of you

---
+++ newbadcommitgenerator.ps1
@@ -0,0 +1,11 @@
+$badcommits = '.','added file xyz','.','.','.','.','.','.','added new file','added new files','deleted file','testing a thing','Fixed stuff','Update' ,'Bug fixes' ,'Changes' ,'WIP' ,'asdf','Try this'
+$x = 50
+# loop thorugh x and create a file with a random commit message
+while($x -gt 0){
+    $x--
+    $commit =   $badcommits | Get-Random
+    $filename = '{0}{1}.txt' -f ($commit -replace '.','summat'), $x
+    New-Item -Path S:\data-scotland-repo-for-commit-messages\ -Name $filename -ItemType file
+    git add .
+    git commit -m $commit
+}
